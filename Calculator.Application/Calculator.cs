1c00de30bf28a28f0a33842586f231f305a74250
Piet-Skiet <joubert.pieter9@gmail.com>
2013-11-04 15:16:04 +0200

    Done

---
+++ Calculator.Application/Calculator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calculator.Application
+{
+    public interface ICalculator
+    {
+        int Add(string input);
+    }
+
+    public class Calculator : ICalculator
+    {
+        List<int> Numbers = new List<int>();
+        List<string> delimiters = new List<string> { ",", "\n" };
+
+        public int Add(string input)
+        {
+            var negativeNumbers = new List<int>();
+            var splitStringdels = new List<string> { "[", "][", "]" };
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+            if (!input.Contains(",") && !input.Contains("\n"))
+            {
+                return Convert.ToInt32(input);
+            }
+            if (input.StartsWith("//["))
+            {
+                if (input.Contains("]["))
+                {
+                    var dells = input.Substring(2, input.IndexOf("]\n", StringComparison.Ordinal) - 1);//                    
+                    string[] multiDels = dells.Split(splitStringdels.ToArray(), StringSplitOptions.None);
+                    foreach (var handleDels in multiDels)
+                    {
+                        delimiters.Add(handleDels);
+                    }
+                }
+                var delimeter = input.Substring(3, input.IndexOf("]\n", StringComparison.Ordinal) - 3);
+                delimiters.Add(delimeter);
+                input = input.Substring(input.IndexOf("]\n", StringComparison.Ordinal) + 2);
+            }
+            else if (input.StartsWith("//"))
+            {
+                string delimeter = input.Substring(2, input.IndexOf("\n", StringComparison.Ordinal) - 2);
+                delimiters.Add(delimeter);
+                input = input.Substring(input.IndexOf('\n')+1);
+            }
+            string[] numersArray = input.Split(delimiters.ToArray(), StringSplitOptions.None);
+            foreach (var init in numersArray.Select(int.Parse))
+            {
+                if (init<0)
+                {
+                    negativeNumbers.Add(init);
+                }
+                else if (init<=1000)
+                {
+                    Numbers.Add(init);
+                }
+                else
+                {
+                    Numbers.Add(0);
+                }
+            }
+            if (negativeNumbers.Any())
+            {
+                const string errormsg = "Negative numbers not allowed: {0}";
+                throw new NegativeNumberException(string.Format(errormsg,string.Join(",",negativeNumbers)));
+            }
+            return Numbers.Sum();
+        }
+    }
+
+    public class NegativeNumberException : Exception
+    {
+        public NegativeNumberException(string error )
+        :base(error){
+        }
+    }
+}
---
+++ Calculator.Test/Test.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Reflection.Emit;
+using Calculator.Application;
+using NUnit.Framework;
+
+
+namespace Calculator.Test
+{
+    [TestFixture]
+    public class Test
+    {
+        private ICalculator _calculator;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _calculator = new Calculator.Application.Calculator();
+        }
+        [Test]
+        public void AnEmptyStringRetunsZero()
+        {
+            Assert.That(_calculator.Add(""),Is.EqualTo(0));
+        }
+        [TestCase ("0",0)]
+        [TestCase ("1",1)]
+        [TestCase ("2",2)]
+        [TestCase ("3",3)]
+        [TestCase ("4",4)]
+        public void OneNumberIsReturnsTheSameValue(string input,int expected)
+        {
+            Assert.That(_calculator.Add(input),Is.EqualTo(expected));
+        }
+        [TestCase ("1,2",3)]
+        [TestCase ("1,2,3",6)]
+        public void TwoNumbersSeparetedByCommaRetunsTheSum(string input,int expected)
+        {
+            Assert.That(_calculator.Add(input),Is.EqualTo(expected));
+        }
+        [TestCase ("1\n2",3)]
+        [TestCase ("3\n4",7)]
+        [TestCase ("7\n8",15)]
+        public void TwoNumbersSeparatedByNewLineReturnsTheSum(string input, int expected)
+        {
+            Assert.That(_calculator.Add(input),Is.EqualTo(expected));
+        }
+        [TestCase("//;\n1;2;3", 6)]
+        [TestCase("//;\n6;7;8", 21)]
+        [TestCase("//;\n21;22;23", 66)]
+        public void NumbersSepartedByCustomDelimeterReturnsTheSum(string input, int expected)
+        {
+            Assert.That(_calculator.Add(input),Is.EqualTo(expected));
+        }
+        [TestCase("//[***]\n1***2***3", 6)]
+        public void CanHandleLongLegnthDelimiters(string input, int expected)
+        {
+            Assert.That(_calculator.Add(input),Is.EqualTo(expected));
+        }
+        [TestCase("1001,2", 2)]
+        [TestCase("10,1002\n3", 13)]
+        [TestCase("//;\n1;1005;2", 3)]
+        [TestCase("//[**]\n10**1020**10", 20)]
+        public void NumbersGreaterThatnAThousandIsIgnored(string input, int expected)
+        {
+            Assert.That(_calculator.Add(input),Is.EqualTo(expected));
+        }
+        [TestCase("//[%][#][@]\n1%2#3@4", 10)]
+        [TestCase("//[*][&][^]\n1*2&3^4", 10)]
+        [TestCase("//[%$][#!][@^]\n1%$2#!3@^4", 10)]
+        public void CanHandleMultipleDelimiters(string input, int expected)
+        {
+            Assert.That(_calculator.Add(input),Is.EqualTo(expected));
+        }
+        [TestCase("-1,2", "Negative numbers not allowed: -1")]
+        [TestCase("//;\n-1;2;-5", "Negative numbers not allowed: -1,-5")]
+        [TestCase("//[***]\n-1***10", "Negative numbers not allowed: -1")]
+        [TestCase("//[***]\n-1***10***-5", "Negative numbers not allowed: -1,-5")]
+        public void NegativeValuesReturnsExceptionMessage(string input, string expected)
+        {
+            Assert.Throws(
+                Is.TypeOf<NegativeNumberException>()
+                    .And.Message.EqualTo(expected), () => _calculator.Add(input));
+        }
+         
+    }
+}
